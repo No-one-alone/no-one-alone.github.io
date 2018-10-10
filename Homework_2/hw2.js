5edd2dd637bd900e2c8b91a3a538656e6dc2fc62
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-10 08:59:58 -0700

    added 'error' html element in index.html file and completed checkInputs function in hw2.js file

--- Homework_2/hw2.js
+++ Homework_2/hw2.js
@@ -1,41 +1,90 @@
 // This is my Javascript file for the Number Checker web page for Homework#2 of CS 460
 // Date finished: 10/10/2018
 
+var compareChecked = false;
 
+var multipleChecked = false;
+
+// This function checks if the inputs are valid
+// and returns an error message to the user if they are not.
 function checkInputs()
 {
-    var firstInput = parseInt(document.getElementById('firstinputnumber').val())
-    /*
-    if(isNaN(firstInput))
-    {
-        var node = document.getElementById("error");
-        var heading = document.createElement("h4");
-        var message = document.createTextNode("Both inputs must be numbers!" + firstInput);
-        node.style.background = "red";
-        node.style.color = "white";
-        node.style.padding = "1px";
-        heading.appendChild(message);
-        node.appendChild(heading);
+    // Extracts the user inputs from the input numbers part.
+    var firstInput = parseInt(document.getElementById('firstinputnumber').value);
+    var secondInput = parseInt(document.getElementById('secondinputnumber').value);
 
+    // This resets the error message if one was already present.
+    $('#error').empty();
+   
+    // This checks if both inputs were numbers
+    // and sends an error message if not.
+    if(isNaN(firstInput) || isNaN(secondInput))
+    {
+       
+        var node = document.getElementById("error"); // retrieves the "error" element
+        var heading = document.createElement("h4");  // creates "heading" element
+        var message = document.createTextNode("Both inputs must be numbers!"); // creates error message.
+        node.style.background = "red"; // sets background of error message.
+        node.style.color = "white"; // sets the color of the letters of the message.
+        //node.style.padding = "1px";
+        heading.appendChild(message); // appends the message to the heading.
+        node.appendChild(heading); // appends the message to the node.
+        
         return false;
     }
-    if(!isNaN(firstInput))
+
+    // This checks if both numbers were within the valid range of 1 to 1000 (inclusive)
+    // and sends an error message if not.
+    if(!isNaN(firstInput) || !isNaN(secondInput))
     {
-        if( firstInput < 1 || firstInput > 1000)
+        if( firstInput < 1 || firstInput > 1000 || secondInput < 1 || secondInput > 1000)
         {
-            var message = document.createTextNode("Both inputs must be between 1 and 1000!");
+            var node = document.getElementById("error"); // retrieves the "error" element
+            var heading = document.createElement("h4"); // creates "heading" element
+            var message = document.createTextNode("Both inputs must be between 1 and 1000!"); // creates error message.
+            node.style.background = "red"; // sets background of error message.
+            node.style.color = "white"; // sets the color of the letters of the message.
+            //node.style.padding = "1px";
+            heading.appendChild(message); // appends the message to the heading.
+            node.appendChild(heading); // appends the message to the node.
+
+       
         }
         return false;
     }
 
-    */
-    return true
+    return true; // If user inputs were valid, then true is returned.
 }
 
+// This function serves as the entry point for
+// the hw2 javascript file.
+// It is responsible for calling its various methods
+// when the user clicks "Analyze"
 function propertyAnalysis()
 {
     if(checkInputs())
     {
+         compareChecked = $('#compare').is(":checked");
+         multipleChecked = $('#multiple').is(":checked");
+
+
+        if(compareChecked == true)
+        {
+            var node = document.getElementById("error"); // retrieves the "error" element
+            var heading = document.createElement("h4");  // creates "heading" element
+            var message = document.createTextNode("Both inputs must be numbers!"); // creates error message.
+            node.style.background = "red"; // sets background of error message.
+            node.style.color = "white"; // sets the color of the letters of the message.
+            //node.style.padding = "1px";
+            heading.appendChild(message); // appends the message to the heading.
+            node.appendChild(heading); // appends the message to the node.
+        }
+
+
+
+
+
+
 
     }
 
