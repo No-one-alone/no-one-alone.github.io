4d84857bdd51bbc64324e68e645386eb4f11c2fb
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-16 13:25:52 -0700

    translated main() function from Main.java file into equivalent in MainClass.cs file

--- Homework_3/ConsoleApp1/ConsoleApp1/MainClass.cs
+++ Homework_3/ConsoleApp1/ConsoleApp1/MainClass.cs
@@ -43,7 +43,44 @@ namespace ConsoleApp1
         }
 
 
-        
+        public static void Main(string[] args)
+        {
+            int n = 10;
+
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Please invoke with the max value to print binary up to, like this:");
+                Console.WriteLine("\tjava Main 12");
+
+                return;
+            }
+
+            try
+            {
+               int.TryParse(args[0], out n);
+            }
+            catch
+            {
+                Console.WriteLine("I'm sorry, I can't understand the number: " + args[0]);
+                return;
+            }
+
+            LinkedList<String> output = GenerateBinaryRepresentationList(n);
+
+            int maxLength = output.Count();
+
+            foreach(string item in output)
+            {
+                for(int i = 0; i < maxLength - item.Length; ++i)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(item);
+
+
+            }
+        }
 
 
 
