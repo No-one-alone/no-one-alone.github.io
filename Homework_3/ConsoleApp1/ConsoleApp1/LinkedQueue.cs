315d1f66f08f8242354f2fbb4b9e1c4a585162ad
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-16 11:38:06 -0700

    finished QueueUnderflowException.cs file regarding translation from QueueUnderflowException.java file and added LinkedQueue.cs file

---
+++ Homework_3/ConsoleApp1/ConsoleApp1/LinkedQueue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LinkedQueue<T> : IQueueInterface<T>
+    {
+        public bool IsEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Push(T element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Homework_3/ConsoleApp1/ConsoleApp1/QueueUnderflowException.cs
+++ Homework_3/ConsoleApp1/ConsoleApp1/QueueUnderflowException.cs
@@ -1,15 +1,29 @@
 ﻿
 
+using System;
+
 namespace ConsoleApp1
 {
     /// <summary>
     /// This is a custom unchecked exception to represent situations where 
     /// an illegal operation was performed on an empty queue.
     /// </summary>
-    public class QueueUnderflowException
+    public class QueueUnderflowException : Exception
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public QueueUnderflowException()
+        {
+        }
 
+        public QueueUnderflowException(string message) : base(message)
+        {
+        }
     }
 
-    
+
+
+
+
 }
