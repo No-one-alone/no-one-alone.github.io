d3ebe53e3f08ff47a051c38a6b783460641a8dab
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-16 12:47:00 -0700

    translated generateBinaryRepresentationList(int n) function from Main.java file into MainClass.cs file

--- Homework_3/ConsoleApp1/ConsoleApp1/MainClass.cs
+++ Homework_3/ConsoleApp1/ConsoleApp1/MainClass.cs
@@ -8,5 +8,46 @@ namespace ConsoleApp1
 {
     public class MainClass
     {
+        static LinkedList<string> GenerateBinaryRepresentationList(int n)
+        {
+            LinkedQueue<StringBuilder> q = new LinkedQueue<StringBuilder>();
+
+            LinkedList<string> output = new LinkedList<string>();
+
+            if( n < 1)
+            {
+                return output;
+            }
+
+            q.Push(new StringBuilder("1"));
+
+            while(n-- > 0)
+            {
+                StringBuilder sb = q.Pop();
+
+                output.AddLast(sb.ToString());
+
+                StringBuilder sbc = new StringBuilder(sb.ToString());
+
+                sb.Append('0');
+
+                q.Push(sb);
+
+                sbc.Append('1');
+
+                q.Push(sbc);
+
+            }
+
+            return output;
+        }
+
+
+        
+
+
+
+
+
     }
 }
