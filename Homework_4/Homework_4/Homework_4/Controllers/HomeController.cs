75fccb1912a9b16300665f67cced6860141a7679
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-23 19:02:58 -0700

    added calculation and case matching code with HttpGet to HomeController.cs file and corrected some errors in Converter.cshtml file

--- Homework_4/Homework_4/Homework_4/Controllers/HomeController.cs
+++ Homework_4/Homework_4/Homework_4/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//added this
+using System.Diagnostics;
+
 namespace Homework_4.Controllers
 {
     public class HomeController : Controller
@@ -13,11 +16,46 @@ namespace Homework_4.Controllers
             return View();
         }
 
-        // changed about to converter for function name
+        // changed "about" to "converter" for function name
+        [HttpGet]
         public ActionResult Converter()
         {
-            ViewBag.Message = "Your application description page.";
+            // ViewBag.Message = "Your application description page.";
 
+            ViewBag.result = false;
+            double miles = Convert.ToDouble(Request.QueryString["mile"]);
+            string measure = Request.QueryString["metric-unit"];
+            Debug.WriteLine(miles);
+            Debug.WriteLine(measure);
+
+            double calcedConversion = 0;
+
+            if(measure == "millimeters")
+            {
+                calcedConversion = miles * 1609344;
+                ViewBag.result = true;
+            }
+            else if(measure == "centimeters")
+            {
+                calcedConversion = miles * 160934.4;
+                ViewBag.result = true;
+            }
+            else if (measure == "meters")
+            {
+                calcedConversion = miles * 1609.344;
+                ViewBag.result = true;
+            }
+            else if (measure == "km")
+            {
+                calcedConversion = miles * 1.609344;
+                ViewBag.result = true;
+            }
+
+
+            string message = "Conversion: " + Convert.ToString(calcedConversion) + " " + measure;
+            ViewBag.Message = message;
+
+            
             return View();
         }
 
