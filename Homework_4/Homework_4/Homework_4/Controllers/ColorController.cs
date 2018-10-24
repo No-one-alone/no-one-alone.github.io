0b880d6c17b08f75dc1475d959207a1b3a17468a
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-24 10:18:47 -0700

    added ColorController.cs file to Controllers folder, added ColorChooser.cshtml file with new Color folder, and edited the Homework_4.csproj, Contact.cshtml, and _Layout.cshtml files

---
+++ Homework_4/Homework_4/Homework_4/Controllers/ColorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework_4.Controllers
+{
+    public class ColorController : Controller
+    {
+        // GET: Color
+        [HttpGet]
+        public ActionResult ColorChooser()
+        {
+            return View();
+        }
+    }
+}
