c4834e8651e3231f32ca9bf7280f442e5a2b42ac
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-25 04:58:38 -0700

    removed unnecessary links, navbar headers, and action links in HomeController.cs, Index.cshtml, and _Layout.cshtml files

--- Homework_4/Homework_4/Homework_4/Controllers/HomeController.cs
+++ Homework_4/Homework_4/Homework_4/Controllers/HomeController.cs
@@ -20,11 +20,5 @@ namespace Homework_4.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
