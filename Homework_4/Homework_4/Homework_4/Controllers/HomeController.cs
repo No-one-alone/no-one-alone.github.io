474fd2a73b6fab966a42236e4e7dffe119770899
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-28 22:57:37 -0700

    added appropriate C# XML and Razor/HTML comments to the HomeController.cs, Converter.cshtml, Index.cshtml, and _Layout.cshtml files

--- Homework_4/Homework_4/Homework_4/Controllers/HomeController.cs
+++ Homework_4/Homework_4/Homework_4/Controllers/HomeController.cs
@@ -1,39 +1,64 @@
-﻿using System;
-//added this
-using System.Diagnostics;
-using System.Web.Mvc;
+﻿
+// This file contains the controller functions for the Index "Home" page, the Converter page, and the ColorChooser page.
 
+using System; // necessary for datatype conversion functions and string methods.
+
+//added this
+using System.Diagnostics; // Required for debugging functions
+using System.Web.Mvc; // required for MVC web
+
+
+/// <summary>
+/// This the namespace for the project which contains the various controllers needed for manipulating user inputs and returning them to the Views.
+/// </summary>
 namespace Homework_4.Controllers
 {
+    /// <summary>
+    /// This the name of the class which actually contains the controller functions. Note the "Controller" extension.
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Controller function for Index page. 
+        /// </summary>
+        /// <returns>Just returns the View unmodified.</returns>
         public ActionResult Index()
         {
             return View();
         }
 
-        // changed "about" to "converter" for function name
+        
+        /// <summary>
+        /// This is the Controller function for the mile distance Converter web page.
+        /// Notice that it uses a GET request to the server.
+        /// </summary>
+        /// <returns>The View with a message: 
+        /// A string with the correct conversion of the supplied mile distance if user input was valid. 
+        /// A string with an error notice for the user if user input was invalid</returns>
         [HttpGet]
         public ActionResult Converter()
         {
-            // ViewBag.Message = "Your application description page.";
-
+            // These are meant to track if the measure and unit inputs were valid or invalid using the ViewBag.
             ViewBag.checkMeasure = false;
             ViewBag.checkUnit = false;
 
 
-            //  double miles = Convert.ToDouble(Request.QueryString["mile"]);
+            // This attempts to obtain the number value of the user inputted mile distance. Note use of the Request object.
             double miles = -1;
             ViewBag.checkMeasure = Double.TryParse(Request.QueryString["mile"], out miles);
 
+            // This attempts to obtain the string value of the user inputted conversion unit. Note use of the Request object.
             string unit = Request.QueryString["metric-unit"];
 
-
+            // These can be used to debug the obtained user inputs.
             Debug.WriteLine(miles);
             Debug.WriteLine(unit);
 
+            // This is used to hold the conversion result.
             double calcedConversion = -1;
 
+            // This code block determines which unit has been specified
+            // and then calculates the appropriate conversion from the miles given.
             if(unit == "millimeters")
             {
                 calcedConversion = miles * 1609344;
@@ -56,8 +81,11 @@ namespace Homework_4.Controllers
             }
 
 
+            // This holds the message to be displayed to the user.
             string message = "";
 
+            // This large code block creates the message to display to the user provided all inputs were valid.
+            // and if any inputs were invalid, then the appropriate error message is generated for display to the user.
             if (ViewBag.checkMeasure == true && ViewBag.checkUnit == true)
             {
                 message = miles + " miles is equal to " + Convert.ToString(calcedConversion) + " " + unit;
@@ -116,16 +144,10 @@ namespace Homework_4.Controllers
             }
 
        
+            // This stores the generated message into the ViewBag for later retrieval by the View.
             ViewBag.Message = message;
         
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(); // This returns the View.
         }
     }
 }
