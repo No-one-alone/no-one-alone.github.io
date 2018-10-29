cf1292fdb393b915eb11a56eea0ac801e25ec1fb
Khorben Boyer <kboyer12@mail.wou.edu>
2018-10-29 01:00:22 -0700

    edited C# code in ColorController.cs file to be in line with C# convention

--- Homework_4/Homework_4/Homework_4/Controllers/ColorController.cs
+++ Homework_4/Homework_4/Homework_4/Controllers/ColorController.cs
@@ -40,17 +40,17 @@ namespace Homework_4.Controllers
         /// <summary>
         /// This is the non-default Controller function for the ColorChooser Option
         /// </summary>
-        /// <param name="FirstColor">The first color submitted by the user</param>
-        /// <param name="SecondColor">The second color submitted by the user</param>
+        /// <param name="firstColor">The first color submitted by the user</param>
+        /// <param name="secondColor">The second color submitted by the user</param>
         /// <returns>Returns the View with the appropriate attributes set to the correct color display or error value</returns>
         [HttpPost]
-        public ActionResult ColorChooser(String FirstColor, string SecondColor)
+        public ActionResult ColorChooser(String firstColor, string secondColor)
         {
             // Extracts the Hexadecimal color code in the first input from the query string
-            FirstColor = Request.Form["first_color"];
+            firstColor = Request.Form["first_color"];
 
             // Extracts the Hexadecimal color code in the second input from the query string
-            SecondColor = Request.Form["second_color"];
+            secondColor = Request.Form["second_color"];
 
             // This is test/debug code.
            // FirstColor = FirstColor.Remove(1, 1);
@@ -62,43 +62,43 @@ namespace Homework_4.Controllers
 
             // This code block creates the mix color result from the two input colors and formats it for display to the View after checking if user input was valid.
             // if user input was invalid, then an error message is prepared for the user instead.
-            if (FirstColor != null && SecondColor != null && Regex.IsMatch(FirstColor, "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") && Regex.IsMatch(SecondColor, "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"))
+            if (firstColor != null && secondColor != null && Regex.IsMatch(firstColor, "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$") && Regex.IsMatch(secondColor, "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"))
             {
                 // this debugging code
-                Debug.WriteLine(FirstColor);
-                Debug.WriteLine(SecondColor);
+                Debug.WriteLine(firstColor);
+                Debug.WriteLine(secondColor);
 
                 // These are variables to store the components that make up the ARGB structure.
-                int AlphaComponent, RedComponent, GreenComponent, BlueComponent;
+                int alphaComponent, redComponent, greenComponent, blueComponent;
 
                 // These translate the HTML color representation to a GDI+ Color structure.
-                Color FirstColor_ARGB = ColorTranslator.FromHtml(FirstColor);
-                Color SecondColor_ARGB = ColorTranslator.FromHtml(SecondColor);
+                Color firstColor_ARGB = ColorTranslator.FromHtml(firstColor);
+                Color secondColor_ARGB = ColorTranslator.FromHtml(secondColor);
 
                 // This code block invokes the following algorithm:
                 // if the sum of the alpha, red, green, or blue color components is equal or greater than 255,
                 // then the color is clamped to the value of 255.
                 // if the summed color value is less than 255, then the summed result is displayed as is.
-                AlphaComponent = FirstColor_ARGB.A + SecondColor_ARGB.A >= 255 ? 255 : FirstColor_ARGB.A + SecondColor_ARGB.A;
+                alphaComponent = firstColor_ARGB.A + secondColor_ARGB.A >= 255 ? 255 : firstColor_ARGB.A + secondColor_ARGB.A;
 
-                RedComponent = FirstColor_ARGB.R + SecondColor_ARGB.R >= 255 ? 255 : FirstColor_ARGB.R + SecondColor_ARGB.R;
+                redComponent = firstColor_ARGB.R + secondColor_ARGB.R >= 255 ? 255 : firstColor_ARGB.R + secondColor_ARGB.R;
 
-                GreenComponent = FirstColor_ARGB.G + SecondColor_ARGB.G >= 255 ? 255 : FirstColor_ARGB.G + SecondColor_ARGB.G;
+                greenComponent = firstColor_ARGB.G + secondColor_ARGB.G >= 255 ? 255 : firstColor_ARGB.G + secondColor_ARGB.G;
 
-                BlueComponent = FirstColor_ARGB.B + SecondColor_ARGB.B >= 255 ? 255 : FirstColor_ARGB.B + SecondColor_ARGB.B;
+                blueComponent = firstColor_ARGB.B + secondColor_ARGB.B >= 255 ? 255 : firstColor_ARGB.B + secondColor_ARGB.B;
 
                 // This creates the new mixed color from the color components generated above.
-                string ColorMixResult = ColorTranslator.ToHtml(Color.FromArgb(AlphaComponent, RedComponent, GreenComponent, BlueComponent ));
+                string colorMixResult = ColorTranslator.ToHtml(Color.FromArgb(alphaComponent, redComponent, greenComponent, blueComponent ));
            
                 // This sets the ViewBag attribute to true to indicate that the View should show a color display an answer.
                 ViewBag.show = true;
 
                 // This code block formats the colors for display in the View along with some additional symbols.
-                ViewBag.firstInputColor = "width: 70px; height: 70px; border: 1px solid #D3D3D3; background:" + FirstColor + ";";
+                ViewBag.firstInputColor = "width: 70px; height: 70px; border: 1px solid #D3D3D3; background:" + firstColor + ";";
 
-                ViewBag.secondInputColor = "width: 70px; height: 70px; border: 1px solid #D3D3D3; background:" + SecondColor + ";";
+                ViewBag.secondInputColor = "width: 70px; height: 70px; border: 1px solid #D3D3D3; background:" + secondColor + ";";
 
-                ViewBag.outputColor = "width: 70px; height: 70px; border: 1px solid #D3D3D3; background:" + ColorMixResult + ";";
+                ViewBag.outputColor = "width: 70px; height: 70px; border: 1px solid #D3D3D3; background:" + colorMixResult + ";";
 
             }
             else // This indicates that an error message should be shown to the user in the View due to invald inputs.
