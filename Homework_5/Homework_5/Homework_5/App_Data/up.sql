fe2bb107b039ef13bc17e12a24731617c9acd92b
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-02 18:38:28 -0700

    added up.sql query file to App_data folder in Homework_5. Included up.sql in project and added code for creating the table for user entries along with seeding it with five complete entries.

---
+++ Homework_5/Homework_5/Homework_5/App_Data/up.sql
@@ -0,0 +1,26 @@
+﻿-- Renters table
+CREATE TABLE [dbo].[Users]
+(
+	[ID]		          INT IDENTITY (1,1)	NOT NULL,
+	[FirstName]	          NVARCHAR(64)		NOT NULL,
+	[LastName]	          NVARCHAR(128)		NOT NULL,
+	[PhoneNumber]         NVARCHAR(128)		NOT NULL,
+	[ApartmentName]       NVARCHAR(128)		NOT NULL,
+	[UnitNumber]          NVARCHAR(128)		NOT NULL,
+	[RequestExplanation]  NVARCHAR(128)		NOT NULL,
+	[Permission]          NVARCHAR(128)		NOT NULL,
+
+	[DateTimeOfRequest]   DateTime			NOT NULL,
+	CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+INSERT INTO [dbo].[Users] (FirstName, LastName, PhoneNumber, ApartmentName, UnitNumber, RequestExplanation, Permission, DateTimeOfRequest) VALUES
+	('Khorben','Boyer','503-522-8328','Ridge','123', 'Water everywhere. FIX IT!', 'Given','2000-08-22 00:00:00'),
+	('Revan','DarkSider','503-540-2382','Korriban','666', 'The Darkside is everywhere!', 'NotGiven','2004-12-01 02:30:20'),
+	('Obiwan','Kenobi','503-451-7729','Coruscant','248', 'Trust in the Force!', 'Given','2001-09-20 00:00:00'),
+	('Mace','Windu','503-630-5841','Genosis','000', 'This party is over!', 'NotGiven','2008-08-13 01:01:03'),
+	('Yoda','...','503-342-6874','Jedi Temple','999', 'Long night, this, will be!', 'Given','1999-12-22 01:20:13')
+	
+GO
+
+--SELECT * FROM dbo.Users
