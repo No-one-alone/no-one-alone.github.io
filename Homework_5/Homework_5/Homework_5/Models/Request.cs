8b6ed61819f03f8a2fb57a1cea5afd237dfafeb7
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-05 15:03:17 -0800

    added properties to Request.cd model class file.

--- Homework_5/Homework_5/Homework_5/Models/Request.cs
+++ Homework_5/Homework_5/Homework_5/Models/Request.cs
@@ -7,5 +7,27 @@ namespace Homework_5.Models
 {
     public class Request
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string ApartmentName { get; set; }
+
+        public int UnitNumber { get; set; }
+
+        public string RequestExplanation { get; set; }
+
+        public bool Permission { get; set; }
+
+        public DateTime DateTimeOfRequest { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {FirstName} {LastName} {PhoneNumber} {ApartmentName} {UnitNumber} {RequestExplanation} {Permission} {DateTimeOfRequest}";
+        }
+
+
     }
 }
