82b52ba676a3e8c1cc2687755c7f65fea8e5960e
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-06 01:29:58 -0800

    added data annotations to properties in Request.cs model class file, added DbContext class for derivation of access property to our database in RequestContext.cs file, and added connection string - to our database - to the web.config file.

--- Homework_5/Homework_5/Homework_5/DAL/RequestContext.cs
+++ Homework_5/Homework_5/Homework_5/DAL/RequestContext.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Homework_5.DAL
 {
-    public class RequestContext
+    public class RequestContext : DbContext
     {
+        public RequestContext() : base("name=OurRenters")
+        {
+
+        }
     }
 }
--- Homework_5/Homework_5/Homework_5/Models/Request.cs
+++ Homework_5/Homework_5/Homework_5/Models/Request.cs
@@ -3,24 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// Added this 
+using System.ComponentModel.DataAnnotations;
+
 namespace Homework_5.Models
 {
     public class Request
     {
+        [Key]
+        public int ID { get; set }
+
+        [Required]
+        [StringLength(20)]
         public string FirstName { get; set; }
 
+        [Required]
+        [StringLength(20)]
         public string LastName { get; set; }
 
+        [Required]
+        [StringLength(12)]
         public string PhoneNumber { get; set; }
 
+        [Required]
+        [StringLength(40)]
         public string ApartmentName { get; set; }
 
+        [Required]
         public int UnitNumber { get; set; }
 
+        [Required]
         public string RequestExplanation { get; set; }
 
+        [Required]
         public bool Permission { get; set; }
 
+        [Required]
         public DateTime DateTimeOfRequest { get; set; }
 
         public override string ToString()
