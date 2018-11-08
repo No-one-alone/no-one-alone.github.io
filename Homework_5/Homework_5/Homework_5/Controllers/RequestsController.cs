896e2f972baa7e8241c66ec69ba59deacee2014c
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-08 11:23:20 -0800

    changed ActionLink in _Layout.cshtml file to use Listing() ActionResult function, added Requests folder and added the Listing.cshtml View file to it, and added the RequestsController.cs controller file to the Controllers folder along with edits to the HomeController.cs file

---
+++ Homework_5/Homework_5/Homework_5/Controllers/RequestsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//Added this
+using Homework_5.DAL;
+using Homework_5.Models;
+
+namespace Homework_5.Controllers
+{
+    public class RequestsController : Controller
+    {
+
+        private RequestContext database = new RequestContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your home page.";
+
+            return View();
+        }
+
+        // GET: Requests
+        //public ActionResult Index()
+        [HttpGet]
+        public ActionResult Listing()
+        {
+            return View(database.Requests.ToList());
+        }
+
+        //GET: Requests/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requests/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FirstName, LastName, PhoneNumber, ApartmentName, UnitNumber, RequestExplanation, Permission, DateTimeOfRequest")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                database.Requests.Add(request);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(request);
+        }
+
+
+
+
+
+        /*
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        */
+
+    }
+}
