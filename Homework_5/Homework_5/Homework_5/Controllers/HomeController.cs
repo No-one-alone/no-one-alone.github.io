38ba1db8af194bab309b6f9dad4c9df944eb0941
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-09 13:27:06 -0800

    finished commenting all relevant files in homework_5 and added blog.md file.

--- Homework_5/Homework_5/Homework_5/Controllers/HomeController.cs
+++ Homework_5/Homework_5/Homework_5/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿// This is the controller file for the home page i.e. our landing page.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+/// <summary>
+/// This is the namespace for our home controller.
+/// </summary>
 namespace Homework_5.Controllers
 {
+
+    /// <summary>
+    /// This is the actual class that defines the home controller.
+    /// </summary>
     public class HomeController : Controller
     {
+
         // GET: Requests
+        /// <summary>
+        /// This is the controller ActionResult function for the home page.
+        /// </summary>
+        /// <returns>It returns the View.</returns>
         public ActionResult Index()
         {
             return View();
--- Homework_5/Homework_5/Homework_5/DAL/RequestContext.cs
+++ Homework_5/Homework_5/Homework_5/DAL/RequestContext.cs
@@ -1,21 +1,39 @@
-﻿using System;
+﻿// This file serves as our data access layer component of the project.
+
+using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+using System.Data.Entity; // This was added via installation of the entity framework.
 using System.Linq;
 using System.Web;
 
-//added this
+//This was added to gain access to our model class.
 using Homework_5.Models;
 
+
+/// <summary>
+/// This is the namespace for our data access layer.
+/// </summary>
 namespace Homework_5.DAL
 {
+
+    /// <summary>
+    /// This is the class that actually defines our data access layer.
+    /// Note the use of the DbContext class.
+    /// </summary>
     public class RequestContext : DbContext
     {
+
+        /// <summary>
+        /// This method allows us access to our named database with respect to web application itself
+        /// </summary>
         public RequestContext() : base("name=OurRenters")
         {
 
         }
 
+        /// <summary>
+        /// Specifies what can be done with database with respect to extracting or inserting data.
+        /// </summary>
         public virtual DbSet<Request> Requests { get; set; }
     }
 }
