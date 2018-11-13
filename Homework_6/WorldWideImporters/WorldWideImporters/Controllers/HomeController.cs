3f4e343e09456b964ccea6b585bf7d1b05bf011e
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-12 19:14:56 -0800

    added an actionresult controller function called Index() with a string parameter to search and find names in database for display and a Details() actionresult controller function to direct users to the details view when a search result name is clicked.

--- Homework_6/WorldWideImporters/WorldWideImporters/Controllers/HomeController.cs
+++ Homework_6/WorldWideImporters/WorldWideImporters/Controllers/HomeController.cs
@@ -4,14 +4,59 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
+// added these
+using WorldWideImporters.Models;
+//using WorldWideImporters.ViewModels;
+
+
 namespace WorldWideImporters.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public WorldWideImportersContext database = new WorldWideImportersContext();
+  
+
+        public ActionResult Index(string query)
         {
-            return View();
+            DisplayModel vm = new DisplayModel();
+
+            if(query == null || query == "")
+            {
+                ViewBag.show = false;
+
+                return View();
+            }
+            else
+            {
+                ViewBag.show = true;
+
+                return View(database.People.Where(x => x.FullName.ToUpper().Contains(query.ToUpper())).ToList());
+            }
+           
         }
+
+
+        public ActionResult Details(int? id)
+        {
+            Person vm = database.People.Find(id);
+
+            return View("Details", vm);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public ActionResult About()
         {
