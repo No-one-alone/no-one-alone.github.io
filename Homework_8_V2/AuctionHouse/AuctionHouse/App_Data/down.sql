6c759803f285d0826f280e53ef756230d4124cbd
Khorben Boyer <kboyer12@mail.wou.edu>
2018-11-29 11:31:13 -0800

    added OurAuctionHouse database to homework 8 project along with up.sql and down.sql files for building and taking down the appropriate tables for the bids, items, buyers, and sellers entities.

---
+++ Homework_8_V2/AuctionHouse/AuctionHouse/App_Data/down.sql
@@ -0,0 +1,11 @@
+﻿-- Takes the tables down
+--DROP TABLE [dbo].[Requests];
+
+ALTER TABLE Items DROP CONSTRAINT [FK_dbo.Items]; -- change name
+ALTER TABLE Bids DROP CONSTRAINT [FK_dbo.Bids]; -- change name
+ALTER TABLE Bids DROP CONSTRAINT [FK2_dbo.Bids]; -- change name
+
+DROP TABLE Buyers;
+DROP TABLE Sellers;
+DROP TABLE Items;
+DROP TABLE Bids;
