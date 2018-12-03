74aabbf2250d0e7f927cbba11af9d1272bceb338
Khorben Boyer <kboyer12@mail.wou.edu>
2018-12-02 19:45:58 -0800

    published homework 8 project on Azure site.

--- Homework_8_V2/AuctionHouse/AuctionHouse/App_Data/down.sql
+++ Homework_8_V2/AuctionHouse/AuctionHouse/App_Data/down.sql
@@ -1,11 +1,15 @@
 ﻿-- Takes the tables down
 --DROP TABLE [dbo].[Requests];
-
 ALTER TABLE Items DROP CONSTRAINT [FK_dbo.Items]; -- change name
+
 ALTER TABLE Bids DROP CONSTRAINT [FK_dbo.Bids]; -- change name
+
 ALTER TABLE Bids DROP CONSTRAINT [FK2_dbo.Bids]; -- change name
 
-DROP TABLE Buyers;
-DROP TABLE Sellers;
-DROP TABLE Items;
-DROP TABLE Bids;
+
+DROP TABLE [dbo].[Bids];
+DROP TABLE [dbo].[Items];
+
+DROP TABLE [dbo].[Buyers];
+DROP TABLE [dbo].[Sellers];
+
