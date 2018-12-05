24628a338d205c47a4a77126e3a338e8ece3a2df
Khorben Boyer <kboyer12@mail.wou.edu>
2018-12-05 00:51:37 -0800

    added code to retrieve sticker from Giphy API using basic initial url completed by secret key from external config file and the word entered by the user.

--- Homework_7/GiphApp/GiphApp/Controllers/AjaxController.cs
+++ Homework_7/GiphApp/GiphApp/Controllers/AjaxController.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Web;
 using System.Web.Mvc;
+
+//added this
+using System.Net;
+using System.IO;
 
 namespace GiphApp.Controllers
 {
     public class AjaxController : Controller
     {
         // GET: Ajax
-        public ActionResult Index()
+        public JsonResult getGIFs(string enteredWord)
         {
-            return View();
+            
+            string uri = "https://api.giphy.com/v1/stickers/translate?api_key=" +
+                         System.Web.Configuration.WebConfigurationManager.AppSettings["key that is used."] +
+                         "&s=" + enteredWord;
+
+            
+            WebRequest webRequest = WebRequest.Create(uri);
+
+            
+            Stream data = webRequest.GetResponse().GetResponseStream();
+
+           
+            var parseData = new System.Web.Script.Serialization.JavaScriptSerializer()
+                                  .DeserializeObject(new StreamReader(data)
+                                  .ReadToEnd());
+
+            
+            return Json(parseData, JsonRequestBehavior.AllowGet);
         }
     }
 }
