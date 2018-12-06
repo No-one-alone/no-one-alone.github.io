638bc7892afb5e9c459bf7fff2d858b96dbde2e4
Khorben Boyer <kboyer12@mail.wou.edu>
2018-12-06 10:00:34 -0800

    completed up.sql and down.sql script files for homework 7 project.

--- Homework_7/GiphApp/GiphApp/App_Data/up.sql
+++ Homework_7/GiphApp/GiphApp/App_Data/up.sql
@@ -1,1 +1,10 @@
-﻿
+﻿CREATE TABLE [dbo].[SearchRequests]
+(
+	[ID]						INT IDENTITY (1,1)	NOT NULL,
+	[RequestWord]				NVARCHAR(120)		NOT NULL,
+	[IPAddressOfRequestor]		NVARCHAR(50)		NOT NULL,
+	[ClientBrowserAgentType] 	NVARCHAR(256)		NOT NULL,
+	[TimeStamp] 				DATETIME			NOT NULL,
+
+	CONSTRAINT [PK_dbo.SearchRequests] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
