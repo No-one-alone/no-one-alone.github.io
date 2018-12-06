5a4c4b1d3ce8cc06635891ea1b981da545bf8ab3
Khorben Boyer <kboyer12@mail.wou.edu>
2018-12-06 10:37:42 -0800

    added model class SearchRequest.cs file under Models folder and added the connection string to the web.config file of the homework 7 project.

---
+++ Homework_7/GiphApp/GiphApp/Models/SearchRequests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+
+// added this
+using System.ComponentModel.DataAnnotations;
+
+namespace GiphApp.Models
+{
+    public class SearchRequests
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public String IPAddressOfRequestor { get; set; }
+
+        [Required]
+        public String RequestWord { get; set; }
+
+      
+        [Required]
+        public String ClientBrowserAgentType { get; set; }
+
+  
+        [Required]
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+
+    }
+}
